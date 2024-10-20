b36bd355a8522e22ae0056de6bd4804403cc5b53
Andrius da Costa Ribas <andriusmao@gmail.com>
2024-10-19 23:16:42 -0300

    Update pt_BR translation.

--- i18n/atsumari_pt_BR.ts
+++ i18n/atsumari_pt_BR.ts
@@ -58,7 +58,7 @@
         <translation>Anéis da Esfera:</translation>
     </message>
     <message>
-        <location filename="../setupwidget.ui" line="477"/>
+        <location filename="../setupwidget.ui" line="483"/>
         <source>Diffuse Color:</source>
         <translation>Cor Difusa:</translation>
     </message>
@@ -68,7 +68,7 @@
         <translation>Tempo para cada iteração:</translation>
     </message>
     <message>
-        <location filename="../setupwidget.ui" line="467"/>
+        <location filename="../setupwidget.ui" line="473"/>
         <source>Light Intensity:</source>
         <translation>Intensidade da Luz:</translation>
     </message>
@@ -83,7 +83,7 @@
         <translation>Cor Ambiente:</translation>
     </message>
     <message>
-        <location filename="../setupwidget.ui" line="437"/>
+        <location filename="../setupwidget.ui" line="443"/>
         <source>Sphere Slices:</source>
         <translation>Fatias da Esfera:</translation>
     </message>
@@ -98,172 +98,183 @@
         <translation>Ações...</translation>
     </message>
     <message>
-        <location filename="../setupwidget.ui" line="500"/>
+        <location filename="../setupwidget.ui" line="506"/>
         <source>Profile:</source>
         <translation>Perfil:</translation>
     </message>
     <message>
-        <location filename="../setupwidget.ui" line="507"/>
+        <location filename="../setupwidget.ui" line="513"/>
         <source>Language:</source>
         <translation>Idioma:</translation>
     </message>
     <message>
-        <location filename="../setupwidget.ui" line="541"/>
+        <location filename="../setupwidget.ui" line="547"/>
         <source>Preview</source>
         <translation>Prévia</translation>
     </message>
     <message>
-        <location filename="../setupwidget.ui" line="552"/>
+        <location filename="../setupwidget.ui" line="558"/>
         <source>Directories</source>
         <translation>Diretórios</translation>
     </message>
     <message>
-        <location filename="../setupwidget.ui" line="561"/>
+        <location filename="../setupwidget.ui" line="567"/>
         <source>Emoji directory:</source>
         <translation>Diretório de emoji:</translation>
     </message>
     <message>
-        <location filename="../setupwidget.ui" line="568"/>
-        <location filename="../setupwidget.ui" line="575"/>
+        <location filename="../setupwidget.ui" line="574"/>
+        <location filename="../setupwidget.ui" line="581"/>
         <source>Select Directory...</source>
         <translation>Selecionar Diretório...</translation>
     </message>
     <message>
-        <location filename="../setupwidget.ui" line="598"/>
+        <location filename="../setupwidget.ui" line="604"/>
         <source>Twitch emote directory:</source>
         <translation>Diretório de emote da Twitch:</translation>
     </message>
     <message>
-        <location filename="../setupwidget.ui" line="606"/>
+        <location filename="../setupwidget.ui" line="612"/>
         <source>Twitch</source>
         <translatorcomment>Plataforma Twitch</translatorcomment>
         <translation>Twitch</translation>
     </message>
     <message>
-        <location filename="../setupwidget.ui" line="615"/>
+        <location filename="../setupwidget.ui" line="621"/>
         <source>Exceptions</source>
         <translation>Exceções</translation>
     </message>
     <message>
-        <location filename="../setupwidget.ui" line="624"/>
+        <location filename="../setupwidget.ui" line="630"/>
         <source>Don&apos;t add emotes/emoji sent from those accounts:</source>
         <translation>Não adicionar emotes ou emoji enviados a partir destas contas:</translation>
     </message>
     <message>
-        <location filename="../setupwidget.ui" line="646"/>
+        <location filename="../setupwidget.ui" line="652"/>
         <source>Add to List</source>
         <translation>Adicionar à Lista</translation>
     </message>
     <message>
-        <location filename="../setupwidget.ui" line="683"/>
+        <location filename="../setupwidget.ui" line="689"/>
         <source>Remove Selected</source>
         <translation>Remover Selecionado</translation>
     </message>
     <message>
-        <location filename="../setupwidget.ui" line="692"/>
+        <location filename="../setupwidget.ui" line="698"/>
         <source>Authentication</source>
         <translation>Autenticação</translation>
     </message>
     <message>
-        <location filename="../setupwidget.ui" line="700"/>
+        <location filename="../setupwidget.ui" line="706"/>
         <source>Twitch Client Id:</source>
         <translation>Client Id da Twitch:</translation>
     </message>
     <message>
-        <location filename="../setupwidget.ui" line="714"/>
+        <location filename="../setupwidget.ui" line="720"/>
         <source>Twitch Dev Console</source>
         <translation>Console Dev da Twitch</translation>
     </message>
     <message>
-        <location filename="../setupwidget.ui" line="725"/>
+        <location filename="../setupwidget.ui" line="731"/>
         <source>Force Twitch authentication on next startup</source>
         <translation>Forçar autenticação da Twitch na próxima inicialização</translation>
     </message>
     <message>
-        <location filename="../setupwidget.ui" line="754"/>
+        <location filename="../setupwidget.ui" line="760"/>
         <source>About</source>
         <translation>Sobre</translation>
     </message>
     <message>
-        <location filename="../setupwidget.ui" line="853"/>
+        <location filename="../setupwidget.ui" line="859"/>
         <source>About Qt...</source>
         <translation>Sobre o Qt...</translation>
     </message>
     <message>
-        <location filename="../setupwidget.ui" line="927"/>
+        <location filename="../setupwidget.ui" line="933"/>
         <source>Save Settings</source>
         <translation>Salvar Configurações</translation>
     </message>
     <message>
-        <location filename="../setupwidget.ui" line="937"/>
+        <location filename="../setupwidget.ui" line="943"/>
         <source>Close this dialog and run</source>
         <translation>Fechar este diálogo e executar</translation>
     </message>
     <message>
-        <location filename="../setupwidget.cpp" line="60"/>
+        <location filename="../setupwidget.cpp" line="767"/>
         <source>New Profile...</source>
         <translation>Novo Perfil...</translation>
     </message>
     <message>
-        <location filename="../setupwidget.cpp" line="61"/>
+        <location filename="../setupwidget.cpp" line="768"/>
         <source>Duplicate this profile...</source>
         <translation>Duplicar este perfil...</translation>
     </message>
     <message>
-        <location filename="../setupwidget.cpp" line="62"/>
+        <location filename="../setupwidget.cpp" line="769"/>
         <source>Rename this Profile</source>
         <translation>Renomear este perfil</translation>
     </message>
     <message>
-        <location filename="../setupwidget.cpp" line="63"/>
+        <location filename="../setupwidget.cpp" line="770"/>
         <source>Delete this Profile</source>
         <translation>Remover este perfil</translation>
     </message>
     <message>
-        <location filename="../setupwidget.cpp" line="254"/>
+        <location filename="../setupwidget.cpp" line="249"/>
         <source>Default</source>
         <translation>Padrão</translation>
     </message>
     <message>
-        <location filename="../setupwidget.cpp" line="345"/>
+        <location filename="../setupwidget.cpp" line="340"/>
         <source>Changing default font</source>
         <translation>Alterando a fonte padrão</translation>
     </message>
     <message>
-        <location filename="../setupwidget.cpp" line="346"/>
+        <location filename="../setupwidget.cpp" line="341"/>
         <source>Changing default font is not advised. SVG Based fonts may not work correctly on Windows, and non-SVG Based fonts may also fail to render correctly on other platforms</source>
         <translation>Não é aconselhado alterar a fonte padrão. Fontes baseadas em SVG podem não funcionar corretamente no Windows, e fontes que não são baseadas em SVG também podem não ser exibidas corretamente em outras plataformas</translation>
     </message>
     <message>
-        <location filename="../setupwidget.cpp" line="366"/>
-        <location filename="../setupwidget.cpp" line="384"/>
+        <location filename="../setupwidget.cpp" line="361"/>
+        <location filename="../setupwidget.cpp" line="391"/>
         <source>Empty ClientId</source>
         <translation>ClientId vazio</translation>
     </message>
     <message>
-        <location filename="../setupwidget.cpp" line="366"/>
-        <location filename="../setupwidget.cpp" line="384"/>
+        <location filename="../setupwidget.cpp" line="361"/>
+        <location filename="../setupwidget.cpp" line="391"/>
         <source>Twitch Client Id is empty, you&apos;ll be not able to connect to your chat, use the Twitch Dev Console button to create a Client on Twitch.</source>
         <translation>O Client Id da Twitch está vazio, não será possível conectar ao seu chat, utilize o botão Console Dev da Twitch para criar um Client na Twitch.</translation>
     </message>
     <message>
-        <location filename="../setupwidget.cpp" line="373"/>
-        <location filename="../setupwidget.cpp" line="451"/>
+        <location filename="../setupwidget.cpp" line="368"/>
+        <location filename="../setupwidget.cpp" line="462"/>
         <source>Success</source>
         <translation>Sucesso</translation>
     </message>
     <message>
-        <location filename="../setupwidget.cpp" line="373"/>
+        <location filename="../setupwidget.cpp" line="368"/>
         <source>Settings saved successfully!</source>
         <translation>Configurações salvas com sucesso!</translation>
     </message>
     <message>
-        <location filename="../setupwidget.cpp" line="415"/>
+        <location filename="../setupwidget.cpp" line="374"/>
+        <location filename="../setupwidget.cpp" line="810"/>
+        <source>There are unsaved changes</source>
+        <translation>Há alterações não salvas</translation>
+    </message>
+    <message>
+        <location filename="../setupwidget.cpp" line="375"/>
+        <source>Do you want to save changes before running?</source>
+        <translation>Você deseja salvar as alterações antes de executar?</translation>
+    </message>
+    <message>
+        <location filename="../setupwidget.cpp" line="426"/>
         <source>Select decoration file</source>
         <translation>Selecione o arquivo de decoração</translation>
     </message>
     <message>
-        <location filename="../setupwidget.cpp" line="415"/>
+        <location filename="../setupwidget.cpp" line="426"/>
         <source>Image Files (*.svg *.png *.jpg *.bmp)</source>
         <translation>Arquivos de Imagem (*.svg *.png *.jpg *.bmp)</translation>
     </message>
@@ -272,89 +283,94 @@
         <translation type="vanished">Arquivos de Imagem (*.png *.jpg *.bmp)</translation>
     </message>
     <message>
-        <location filename="../setupwidget.cpp" line="438"/>
+        <location filename="../setupwidget.cpp" line="449"/>
         <source>Select emotes directory</source>
         <translation>Selecionar o diretório de emotes</translation>
     </message>
     <message>
-        <location filename="../setupwidget.cpp" line="443"/>
+        <location filename="../setupwidget.cpp" line="454"/>
         <source>Select emojis directory</source>
         <translation>Selecionar o diretório de emojis</translation>
     </message>
     <message>
-        <location filename="../setupwidget.cpp" line="451"/>
+        <location filename="../setupwidget.cpp" line="462"/>
         <source>Authentication token has been reset! Twitch login will be required on next startup.</source>
         <translation>O token de autenticação foi redefinido! Será necessário logar na Twitch na próxima inicialização.</translation>
     </message>
     <message>
-        <location filename="../setupwidget.cpp" line="579"/>
+        <location filename="../setupwidget.cpp" line="590"/>
         <source>New Profile</source>
         <translation>Novo Perfil</translation>
     </message>
     <message>
-        <location filename="../setupwidget.cpp" line="580"/>
-        <location filename="../setupwidget.cpp" line="631"/>
-        <location filename="../setupwidget.cpp" line="670"/>
+        <location filename="../setupwidget.cpp" line="591"/>
+        <location filename="../setupwidget.cpp" line="642"/>
+        <location filename="../setupwidget.cpp" line="681"/>
         <source>Profile name:</source>
         <translation>Nome do Perfil:</translation>
     </message>
     <message>
-        <location filename="../setupwidget.cpp" line="581"/>
+        <location filename="../setupwidget.cpp" line="592"/>
         <source>Profile</source>
         <translation>Perfil</translation>
     </message>
     <message>
-        <location filename="../setupwidget.cpp" line="588"/>
-        <location filename="../setupwidget.cpp" line="639"/>
-        <location filename="../setupwidget.cpp" line="678"/>
+        <location filename="../setupwidget.cpp" line="599"/>
+        <location filename="../setupwidget.cpp" line="650"/>
+        <location filename="../setupwidget.cpp" line="689"/>
         <source>Profile already exists!</source>
         <translation>O perfil já existe!</translation>
     </message>
     <message>
-        <location filename="../setupwidget.cpp" line="588"/>
-        <location filename="../setupwidget.cpp" line="639"/>
-        <location filename="../setupwidget.cpp" line="678"/>
+        <location filename="../setupwidget.cpp" line="599"/>
+        <location filename="../setupwidget.cpp" line="650"/>
+        <location filename="../setupwidget.cpp" line="689"/>
         <source>A profile named &apos;%1&apos; already exists. Try a different name.</source>
         <translation>Um perfil chamado &apos;%1&apos; já existe. Tente um nome diferente.</translation>
     </message>
     <message>
-        <location filename="../setupwidget.cpp" line="630"/>
+        <location filename="../setupwidget.cpp" line="641"/>
         <source>Duplicate Profile</source>
         <extracomment>Note to translators: this refers to the action of copying/duplicating a profile</extracomment>
         <translation>Duplicar Perfil</translation>
     </message>
     <message>
-        <location filename="../setupwidget.cpp" line="632"/>
+        <location filename="../setupwidget.cpp" line="643"/>
         <source>Copy of %1</source>
         <translation>Cópia de %1</translation>
     </message>
     <message>
-        <location filename="../setupwidget.cpp" line="669"/>
+        <location filename="../setupwidget.cpp" line="680"/>
         <source>Rename Profile</source>
         <translation>Renomear Perfil</translation>
     </message>
     <message>
-        <location filename="../setupwidget.cpp" line="701"/>
+        <location filename="../setupwidget.cpp" line="712"/>
         <source>Cannot delete profile</source>
         <translation>Não é possível remover o perfil</translation>
     </message>
     <message>
-        <location filename="../setupwidget.cpp" line="702"/>
+        <location filename="../setupwidget.cpp" line="713"/>
         <source>Cannot delete the only remaining profile.</source>
         <translation>Não é possível remover o único perfil restante.</translation>
     </message>
     <message>
-        <location filename="../setupwidget.cpp" line="709"/>
+        <location filename="../setupwidget.cpp" line="720"/>
         <source>Delete Profile</source>
         <translation>Remover Perfil</translation>
     </message>
     <message>
-        <location filename="../setupwidget.cpp" line="710"/>
+        <location filename="../setupwidget.cpp" line="721"/>
         <source>Are you sure you want to delete profile &apos;%1&apos;?</source>
         <translation>Você tem certeza de que deseja remover o perfil &apos;%1&apos;?</translation>
     </message>
     <message>
-        <location filename="../setupwidget.cpp" line="761"/>
+        <location filename="../setupwidget.cpp" line="811"/>
+        <source>Do you want to save changes before closing?</source>
+        <translation>Você deseja salvar as alterações antes de fechar?</translation>
+    </message>
+    <message>
+        <location filename="../setupwidget.cpp" line="832"/>
         <source>About Qt</source>
         <translation>Sobre o Qt</translation>
     </message>
@@ -362,7 +378,7 @@
 <context>
     <name>TwitchAuthFlow</name>
     <message>
-        <location filename="../twitchauthflow.cpp" line="185"/>
+        <location filename="../twitchauthflow.cpp" line="183"/>
         <source>Twitch Authorization</source>
         <translation>Autorização na Twitch</translation>
     </message>
